ccf5f6b0175c6b19cdffa8f3f12e491d84c282a7
Sunil Varghese <sunilbrown@gmail.com>
2013-02-13 14:15:37 -0600

    Create a first bundle

---
+++ FirstBundle/src/main/java/org/sunilbrown/osgi/Hello.java
@@ -0,0 +1,5 @@
+package org.sunilbrown.osgi;
+
+public interface Hello{
+    String sayHello(String name);
+}
---
+++ FirstBundle/src/main/java/org/sunilbrown/osgi/HelloClient.java
@@ -0,0 +1,47 @@
+package org.sunilbrown.osgi;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Validate;
+import org.apache.felix.ipojo.annotations.Instantiate;
+
+@Component(name="AnnotedHelloClient", immediate=true)
+@Instantiate
+    public class HelloClient implements Runnable {
+	
+	@Requires
+	    private Hello[] m_hello; // Service Dependency
+	
+	private final static int DELAY=10000;
+	private boolean end;
+	
+	public void run() {
+	    while (!end) {
+		try {
+		    invoke();
+		    Thread.sleep(DELAY);
+		} catch (InterruptedException ie) { }
+		/* will recheck end */
+	    }
+	}
+	
+	public void invoke() {
+	    for (int i = 0; i < m_hello.length; i++) { 
+		System.out.println(m_hello[i].
+				   sayHello("Clement")); 
+	    }
+	}
+	
+	@Validate
+	    public void starting() {    
+	    Thread T = new Thread(this);     
+	    end = false;     
+	    T.start(); 
+	}
+	
+	@Invalidate
+	    public void stopping() {    
+	    end = true; 
+	}
+    }
